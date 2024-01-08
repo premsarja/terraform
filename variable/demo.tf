c9a75a48bbdba2359c67577ce6757587ae855c27
premsarja <premsagar@prodot.in>
2024-01-08 18:18:15 +0530

    done

--- variable/demo.tf
+++ variable/demo.tf
@@ -1,14 +1,14 @@
 # declaring variable 
-variable "sample" {
+variable "prem" {
     default = "hello world"
 }
 
 #printing variable
 output "op" {
-    value =  var.sample
+    value =  var.prem
 }
 
 #wenever you want to print the variable in the group of words, you need to use the Bash ayntax:
 output "ops" {
-    value = "my first msg is ${var.sample}"
+    value = "my first msg is ${var.prem}"
 }
