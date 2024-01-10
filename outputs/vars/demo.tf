ec5f94d0ce63543b6096bff7823d26f53c5284d2
premsarja <premsagar@prodot.in>
2024-01-10 12:15:12 +0530

    done

--- outputs/vars/demo.tf
+++ outputs/vars/demo.tf
@@ -9,3 +9,15 @@ output "op" {
 output "ops1" {
   value = "welcome to the ${var.prem}"
 }
+
+variable "sagar" {
+  default = [   
+    "my",
+    "country",
+    "india",
+  ]
+}
+
+output "op2" {
+  value = "i am ${var.sagar[0]} i m in ${var.sagar[1]} my country is ${var.sagar[2]}"
+}
