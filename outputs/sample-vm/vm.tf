48054803d3ee944a6deab6bd049e547896b5da14
premsarja <premsagar@prodot.in>
2024-01-11 18:35:44 +0530

    done

--- outputs/sample-vm/vm.tf
+++ outputs/sample-vm/vm.tf
@@ -1,7 +1,22 @@
+#resource "aws_instance" "aws" {
+#  
+#  ami                    = "ami-023c11a32b0207432"
+#  instance_type          = "t3.micro"
+#
+#  tags = {
+#    Name = "prem-tf-instance"
+#  }
+#}
+#
+
+provider "aws" {
+  region = "us-east-1"
+  # Other provider configurations, such as access_key, secret_key, etc., can be added here.
+}
+
 resource "aws_instance" "aws" {
-  
-  ami                    = "ami-023c11a32b0207432"
-  instance_type          = "t3.micro"
+  ami           = "ami-023c11a32b0207432"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "prem-tf-instance"
