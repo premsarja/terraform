26d35ea103dfa6db3e4973711d9bb3f429f765b6
premsarja <premsagar@prodot.in>
2024-01-11 18:36:34 +0530

    done

--- outputs/sample-vm/vm.tf
+++ outputs/sample-vm/vm.tf
@@ -14,7 +14,7 @@ provider "aws" {
   # Other provider configurations, such as access_key, secret_key, etc., can be added here.
 }
 
-resource "aws_instance" "aws" {
+resource "aws_instance" "web" {
   ami           = "ami-023c11a32b0207432"
   instance_type = "t3.micro"
 
