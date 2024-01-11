8a83851ffae01c12da7ce9007c89e75397601e65
premsarja <premsagar@prodot.in>
2024-01-11 12:44:09 +0530

    done

--- outputs/sample-vm/vm.tf
+++ outputs/sample-vm/vm.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "aws" {
   
   ami                    = "ami-023c11a32b0207432"
   instance_type          = "t3.micro"
-#   vpc_security_group_ids = ["sg-12345678"]
+
   tags = {
-    name = "my-tf-instance"
+    Name = "my-tf-instance"
   }
 }
