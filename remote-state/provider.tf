cf1b71d017ca3b13d13221fba711dfaf580a026f
premsarja <premsagar@prodot.in>
2024-01-12 16:23:36 +0530

    done

--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -1,4 +1,4 @@
-provider "aws" {}
+provider "aws" {
 
 terraform {
   backend "s3" {
@@ -6,4 +6,5 @@ terraform {
     key            = "dev/ec2/terraform.tfstate"
     region         = "us-east-1"
   }
+ }
 }
