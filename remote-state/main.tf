aa97b85681a5ed3cd44c804a3bf4e705c107e1bc
premsarja <premsagar@prodot.in>
2024-01-13 09:05:48 +0530

    done

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "sarja" {
+  count   = 2
   ami           = "ami-023c11a32b0207432"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "terraform-sarja-instance"
+    Name = "terraform-sarja-instance ${count.index}"
   }
 }
+
