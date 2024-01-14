fa6bd4ae7fcb2661ea58f5ef9b61bb1d66e1d89d
premsarja <premsagar@prodot.in>
2024-01-14 13:12:27 +0530

    done

--- ami/main.tf
+++ ami/main.tf
@@ -5,7 +5,7 @@ data "aws_ami" "ami" {
 }
 
 resource "aws_instance" "example" {
-  count          = "length(var.howmanyyouwant)"
+  count          = length(var.howmanyyouwant)
   ami            = data.aws_ami.ami.id  # Corrected reference to AMI ID
   instance_type  = "t3.micro"
 
