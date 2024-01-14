15f2b8b00d31ec92ce5d55d5a9e7ae47760e3d40
premsarja <premsagar@prodot.in>
2024-01-14 18:05:59 +0530

    done

--- ami/main.tf
+++ ami/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "ami" {
   instance_type  = "t3.micro"
 
   tags = {
-    Name = "${var.howmanyyouwant${count.index}}"
+    Name = "${var.howmanyyouwant[count.index]}"
   }
 }
 
