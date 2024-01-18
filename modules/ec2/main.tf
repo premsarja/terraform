afa993273f9b9cfd1291b954e4b3c4d1f095b534
premsarja <premsagar@prodot.in>
2024-01-18 15:22:05 +0530

    done

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,5 @@
 # creates ec2 
-resource "aws_instance" "example_instance" {
+resource "aws_instance" "ex" {
   ami             = "ami-033c331c5ccbea9fd"  # Replace with your desired AMI ID
   instance_type   = "t2.micro"
   key_name        = "your-key-pair-name"     # Replace with your key pair name
