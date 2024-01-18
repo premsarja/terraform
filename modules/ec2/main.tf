aa0d008fb99db6639000035492ede0067b33e65f
premsarja <premsagar@prodot.in>
2024-01-18 16:31:10 +0530

    done

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,11 +2,17 @@
 resource "aws_instance" "ex" {
   ami             = "ami-033c331c5ccbea9fd"  # Replace with your desired AMI ID
   instance_type   = "t2.micro"
-  vpc_security_group_ids = var.sg
+  vpc_security_group_ids = [var.sg.sg]
 
   tags = {
     Name = "ex"
   }
 }
 
-variable "sg" {}
+# variable "sg" {}
+
+variable "sg" {
+  type = object({
+    sg = set(string)
+  })
+}
