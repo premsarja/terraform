f84dc524f8ab4753815a95dab2f426579910a6c8
premsarja <premsagar@prodot.in>
2024-01-18 11:14:09 +0530

    done

---
+++ modules/ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "example_instance" {
+  ami             = "ami-xxxxxxxxxxxxxxxxx"  # Replace with your desired AMI ID
+  instance_type   = "t2.micro"
+  key_name        = "your-key-pair-name"     # Replace with your key pair name
+  vpc_security_group_ids = [aws_security_group.sagar.id]
+
+  tags = {
+    Name = "example-instance"
+  }
+}
