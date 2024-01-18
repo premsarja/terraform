05a6f9e37697b32f290919039f08473b684fe959
premsarja <premsagar@prodot.in>
2024-01-18 14:08:36 +0530

    done

--- modules/main.tf
+++ modules/main.tf
@@ -1,10 +1,11 @@
 #calling ec2 module
 
-module "ec2" {                        #any name we can give but source should br correct
+module "ec2" {                        # any name we can give but source should br correct
   source = "./ec2"
-  sgid   = module.sg.sgid
+  sg  = module.sgid
 }
 
 module "aws_security_group" {
   source = "./security_group"
 }
+
