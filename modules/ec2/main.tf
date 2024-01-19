46d862a6a1ff411b7b62954d813d60d339b2d1ce
premsarja <premsagar@prodot.in>
2024-01-19 15:31:12 +0530

    done

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,6 @@
 # creates ec2 
 resource "aws_instance" "example" {
+  count           =         5
   ami             = "ami-033c331c5ccbea9fd"  # Replace with your desired AMI ID
   instance_type   = "t2.micro"
   vpc_security_group_ids = [var.sg]
